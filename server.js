99414e6f5a7d47545d85b546d4a728399fd44040
unknown <suraz.kuru@gmail.com>
2024-09-21 20:23:57 +0545

    portfolio completed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("dotenv").config()
 const dbConfig = require("./config/dbConfig")
 const app = express()
+const path = require('path')
 const cors = require('cors');
 app.use(cors());
 
@@ -11,12 +12,15 @@ app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute)
 
+// deployment
+
 const PORT = process.env.PORT || 4000
 // dbConfig()
 // deployment
-const path = require("path")
+// const path = require("path")
 if(process.env.NODE_ENV === "production")
 {
+    const dirPath = path.resolve()
     app.use(express.static(path.join(__dirname, "client/build")))
     app.get("*", (req, res)=>{
         res.sendFile(path.join(__dirname, "client/build/index.html"))
